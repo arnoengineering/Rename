9b18a03a49c81801289a7e94aee59eb9458b0c96
arno <parnoclaassens@gmail.com>
2020-11-20 09:22:55 -0700

    hi

--- Photo Split.py
+++ Photo Split.py
@@ -4,10 +4,13 @@ from PIL import Image, UnidentifiedImageError
 path = r'C:\Users\parno\Desktop\La Pas'
 new_dir = r'C:\Users\parno\Desktop\LA S'
 size = (200, 400)  # x, y pix
+third_val = {(15, 620): (0, 900), (630, 1240): (0, 900), (125, 1020): (910, 1500)}
+norm_val = [[(15, 540), (560, 1100)], [(0, 750), (760, 1500)]]
 
 
 # third if photo to be split by 3
 def split_files(file_path, third=False):
+    split_rec = []
     if file_path.endswith('.NEF') or file_path.lower().endswith('.jpg'):
         num = 0  # what sub
         try:
@@ -17,34 +20,27 @@ def split_files(file_path, third=False):
             parent_dir = os.path.dirname(file_path).split('\\')[-1]
 
             if third:
-                for x in [0, 1, 0.5]:
-                    if x != 0.5:
-                        y = 0
-                    else:
-                        y = 1
-                    sub_rec = (x * size[0] // 2, y * size[1] // 2,
-                               (x + 1) * size[0] // 2, (y + 1) * size[1] // 2)
-                    sub_img = img.crop(sub_rec)  # put in loop  # todo make function
+                for x_tup, y_tup in third_val.items():
+                    x, x_max = x_tup
+                    y, y_max = y_tup
+                    sub_rec = (x, y, x_max, y_max)
+                    split_rec.append(sub_rec)
+            else:
+                # loops through x and y values in 2x2 grid
+                for x, x_max in norm_val[0]:
+                    for y, y_max in norm_val[1]:
+                        num += 1
+                        sub_rec = (x, y, x_max, y_max)
+                        split_rec.append(sub_rec)
+
+                for rec in split_rec:
+                    sub_img = img.crop(rec)  # put in loop
                     save_dir = os.path.join(new_dir, parent_dir)
                     # makes dir if not present
                     if not os.path.exists(save_dir):
                         os.makedirs(save_dir)
+                    sub_img.save(os.path.join(new_dir, img_name))  # names in new dir
 
-                sub_img.save(os.path.join(new_dir, img_name))  # names in new dir
-            else:
-                # loops through x and y values in 2x2 grid
-                for x in range(1):
-                    for y in range(1):
-                        num += 1
-                        sub_rec = (x * size[0] // 2, y * size[1] // 2,
-                                   (x + 1) * size[0] // 2, (y + 1) * size[1] // 2)  # min max by floor
-                        sub_img = img.crop(sub_rec)  # put in loop
-                        save_dir = os.path.join(new_dir, parent_dir)
-                        # makes dir if not present
-                        if not os.path.exists(save_dir):
-                            os.makedirs(save_dir)
-
-                        sub_img.save(os.path.join(new_dir, img_name))  # names in new dir
         except UnidentifiedImageError:
             print('error with image ', file)
 
