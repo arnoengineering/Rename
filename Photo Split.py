9bd4d52c7a584c8ec1d097f2915635cce17e69ad
arno <parnoclaassens@gmail.com>
2020-11-20 09:31:50 -0700

    hi

--- Photo Split.py
+++ Photo Split.py
@@ -6,10 +6,13 @@ new_dir = r'C:\Users\parno\Desktop\LA S'
 size = (200, 400)  # x, y pix
 third_val = {(15, 620): (0, 900), (630, 1240): (0, 900), (125, 1020): (910, 1500)}
 norm_val = [[(15, 540), (560, 1100)], [(0, 750), (760, 1500)]]
+total = 0
+error = 0
 
 
 # third if photo to be split by 3
 def split_files(file_path, third=False):
+    global error
     split_rec = []
     if file_path.endswith('.NEF') or file_path.lower().endswith('.jpg'):
         num = 0  # what sub
@@ -29,23 +32,29 @@ def split_files(file_path, third=False):
                 # loops through x and y values in 2x2 grid
                 for x, x_max in norm_val[0]:
                     for y, y_max in norm_val[1]:
-                        num += 1
                         sub_rec = (x, y, x_max, y_max)
                         split_rec.append(sub_rec)
 
-                for rec in split_rec:
-                    sub_img = img.crop(rec)  # put in loop
-                    save_dir = os.path.join(new_dir, parent_dir)
-                    # makes dir if not present
-                    if not os.path.exists(save_dir):
-                        os.makedirs(save_dir)
-                    sub_img.save(os.path.join(new_dir, img_name))  # names in new dir
+            for rec in split_rec:
+                num += 1
+                new_name = img_name + ' ' + str(num)
+                sub_img = img.crop(rec)  # put in loop
+                save_dir = os.path.join(new_dir, parent_dir)
+                # makes dir if not present
+                if not os.path.exists(save_dir):
+                    os.makedirs(save_dir)
+                sub_img.save(os.path.join(new_dir, new_name))  # names in new dir
 
         except UnidentifiedImageError:
+            error += 1
             print('error with image ', file)
+        return num
 
 
 for root, dirs, files in os.walk(path):
     for file in files:
         file = os.path.join(root, file)
-        split_files(file)
+        nu = split_files(file)
+        total += nu
+
+print('finished editing {} images, with {} errors'.format(total, error))
