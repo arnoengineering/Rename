dbceeafc1b8310ad7b04ceb15c5babe8fe866e65
arno <parnoclaassens@gmail.com>
2020-11-19 18:31:26 -0700

    hi

--- Img rotate.py
+++ Img rotate.py
@@ -4,10 +4,10 @@ from PIL import Image
 
 path = ''
 file_dir = os.listdir(path)
-ang = input('angle CCW')  # angle from dir
+ang = int(input('angle CCW: '))  # angle from dir
 
 for file in file_dir:
     file = os.path.join(path, file)
     img = Image.open(file)
     img = img.rotate(ang)
-    img.save(file)  # todo file or f_path
+    img.save(file)
--- Photo Split.py
+++ Photo Split.py
@@ -3,22 +3,31 @@ from PIL import Image
 
 path = ''
 new_dir = ''
-sie = (200, 400)  # x, y pix
+size = (200, 400)  # x, y pix
 for root, dirs, files in os.walk(path):
-    for file in files:
+    for d in dirs:
         pass
 
 
 def split_files(file_path, img_size):
     num = 0  # what sub
     img = Image.open(file_path)
+    # gets img name and basename
     img_name = os.path.basename(file_path)
+    parent_dir = os.path.dirname(file_path).split('\\')[-1]
+
+    # loops through x and y valuse in 2x2 grid
     for x in range(1):
         for y in range(1):
             num += 1
             sub_rec = (x * img_size[0] // 2, y * img_size[1] // 2,
-                       (x + 1) * img_size[0] // 2, (y + 1) * img_size[1] // 2)
+                       (x + 1) * img_size[0] // 2, (y + 1) * img_size[1] // 2)  # min max by floor
             sub_img = img.crop(sub_rec)  # put in loop
-            sub_img.save(os.path.join(new_dir, img_name))  # naves in new dir  # todo where to save
+            save_dir = os.path.join(new_dir, parent_dir)
+            # makes dir if not present
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+
+            sub_img.save(os.path.join(new_dir, img_name))  # names in new dir
 
 
