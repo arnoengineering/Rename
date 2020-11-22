d09923dd0a95a9c58cd13c21e6875e5db166d0b6
arno <parnoclaassens@gmail.com>
2020-11-21 19:59:48 -0700

    hi

--- Img rotate.py
+++ Img rotate.py
@@ -3,16 +3,18 @@ import os
 from PIL import Image
 
 path = r'N:\P + V\Mom Split'
-for root, dirs, files in path:
+for root, dirs, files in os.walk(path):
     for file in files:
-        parent_dir, ang_dir = root.split('\\')[-2:]
-        try:
-            ang = int(ang_dir)
-        except ValueError:  # todo what error
-            print('error with dir ', root)
-            break
-        else:
-            if ang != 0:
+        if file.endswith('.jpg'):
+            parent_dir, ang_dir = root.split('\\')[-2:]  # dir for name and angle
+            try:
+                ang = int(ang_dir)
+            except ValueError:  # todo what error
+                print('error with dir ', root)
+                break  # stops rerunning code for same albs if already checked
+            else:
+                if ang == 0:
+                    break
                 img = Image.open(os.path.join(root, file))
                 img = img.rotate(ang)
-                img.save(os.path.join(parent_dir, file))
+                img.save(os.path.join(parent_dir, file))  # moves to original dir
